f49a186e84023cf97bf94706fb145e3c51fa2ae5
silnavar <silvana.navarro@haulmer.com>
2025-04-05 20:59:33 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-variable_entorno = os.getenv("CREDENTIALS_GC")
+variable_entorno = os.getenv("GITHUB_TOKEN")
 with open("salida.txt","w") as archivo:
   archivo.write(variable_entorno)

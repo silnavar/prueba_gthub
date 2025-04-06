627abdda1f05106382f28ff756dd0ab7107840f4
silnavar <silvana.navarro@haulmer.com>
2025-04-05 21:10:53 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,6 +1,12 @@
 import sys
 import os
 
-variable_entorno = os.getenv("GITHUB_TOKEN")
-with open("salida.txt","w") as archivo:
-  archivo.write(variable_entorno)
+credencial = "GITHUB_TOKEN"
+variable_entorno = os.getenv(credencial)
+if credencial in locals():
+    print(variable_entorno)
+    with open("salida.txt","w") as archivo:
+        archivo.write(variable_entorno)
+else:
+    with open("salida.txt","w") as archivo:
+        archivo.write(f'Variable {credencial} no existe')

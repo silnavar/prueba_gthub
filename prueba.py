9af26ce4199ce0f0c06d27dff16e04bfac757f81
silnavar <silvana.navarro@haulmer.com>
2025-04-05 23:19:25 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -4,9 +4,8 @@ import os
 credencial = "GITHUB_TOKEN"
 variable_entorno = os.getenv(credencial)
 if variable_entorno is not None:
-    print(variable_entorno)
     with open("salida.txt","w") as archivo:
-        archivo.write(variable_entorno)
+        archivo.write(str(variable_entorno))
 else:
     with open("salida.txt","w") as archivo:
         archivo.write(f'Variable {credencial} no existe')
